ab9f569121544783bbe1f711f92f239588eccbef
guanzhihua1992 <18300004793@163.com>
2023-06-13 20:38:11 +0800

    wordcount

--- apache-flink/src/main/java/org/apache/flink/examples/java/wordcount/WordCount.java
+++ apache-flink/src/main/java/org/apache/flink/examples/java/wordcount/WordCount.java
@@ -1,12 +1,13 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,20 +16,20 @@
  * limitations under the License.
  */
 
-package org.apache.flink.streaming.examples.wordcount;
+package org.apache.flink.examples.java.wordcount;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.MultipleParameterTool;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.examples.wordcount.util.WordCountData;
+import org.apache.flink.examples.java.wordcount.util.WordCountData;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.Preconditions;
 
 /**
  * Implements the "WordCount" program that computes a simple word occurrence histogram over text
- * files in a streaming fashion.
+ * files.
  *
  * <p>The input is a plain text file with lines separated by newline characters.
  *
@@ -38,30 +39,29 @@ import org.apache.flink.util.Preconditions;
  * <p>This example shows how to:
  *
  * <ul>
- *   <li>write a simple Flink Streaming program,
- *   <li>use tuple data types,
+ *   <li>write a simple Flink program.
+ *   <li>use Tuple data types.
  *   <li>write and use user-defined functions.
  * </ul>
  */
 public class WordCount {
 
     // *************************************************************************
-    // PROGRAM
+    //     PROGRAM
     // *************************************************************************
 
     public static void main(String[] args) throws Exception {
 
-        // Checking input parameters
         final MultipleParameterTool params = MultipleParameterTool.fromArgs(args);
 
         // set up the execution environment
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
         // make parameters available in the web interface
         env.getConfig().setGlobalJobParameters(params);
 
         // get input data
-        DataStream<String> text = null;
+        DataSet<String> text = null;
         if (params.has("input")) {
             // union all the inputs from text files
             for (String input : params.getMultiParameterRequired("input")) {
@@ -71,34 +71,34 @@ public class WordCount {
                     text = text.union(env.readTextFile(input));
                 }
             }
-            Preconditions.checkNotNull(text, "Input DataStream should not be null.");
+            Preconditions.checkNotNull(text, "Input DataSet should not be null.");
         } else {
+            // get default test text data
             System.out.println("Executing WordCount example with default input data set.");
             System.out.println("Use --input to specify file input.");
-            // get default test text data
-            text = env.fromElements(WordCountData.WORDS);
+            text = WordCountData.getDefaultTextLineDataSet(env);
         }
 
-        DataStream<Tuple2<String, Integer>> counts =
+        DataSet<Tuple2<String, Integer>> counts =
                 // split up the lines in pairs (2-tuples) containing: (word,1)
                 text.flatMap(new Tokenizer())
                         // group by the tuple field "0" and sum up tuple field "1"
-                        .keyBy(value -> value.f0)
+                        .groupBy(0)
                         .sum(1);
 
         // emit result
         if (params.has("output")) {
-            counts.writeAsText(params.get("output"));
+            counts.writeAsCsv(params.get("output"), "\n", " ");
+            // execute program
+            env.execute("WordCount Example");
         } else {
             System.out.println("Printing result to stdout. Use --output to specify output path.");
             counts.print();
         }
-        // execute program
-        env.execute("Streaming WordCount");
     }
 
     // *************************************************************************
-    // USER FUNCTIONS
+    //     USER FUNCTIONS
     // *************************************************************************
 
     /**
