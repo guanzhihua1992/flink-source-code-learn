437b65039e0674c23be854a5d481bb38a5a4293c
guanzhihua1992 <18300004793@163.com>
2023-06-22 10:03:56 +0800

    add tables

---
+++ apache-flink/src/main/java/org/apache/flink/examples/java/table/basics/UpdatingTopCityExample.java
@@ -0,0 +1,2 @@
+package org.apache.flink.examples.java.table.basics;public class UpdatingTopCityExample {
+}
---
+++ apache-flink/src/main/java/org/apache/flink/examples/java/table/connectors/SocketSourceFunction.java
@@ -0,0 +1,2 @@
+package org.apache.flink.examples.java.table.connectors;public class SocketSourceFunction {
+}
